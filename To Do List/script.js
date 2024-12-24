03560691b148fa6e5c40c5df592f534f0729955e
Heena Rathod <rathodheenak1@gmail.com>
2024-12-24 19:53:29 +0530

    added two projects to do list and notes

---
+++ To Do List/script.js
@@ -0,0 +1,41 @@
+const input = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addTask() {
+  if (input.value === "") {
+    alert("You must write somthing");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  input.value = "";
+  saveData();
+}
+
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showTask() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
